1e4a3aed30bf714bb7fdb12a7a5c199d6d8d2a65
Brian Drawert <bdrawert@cs.ucsb.edu>
2016-10-16 13:12:43 -0700

    bug fix

--- MolnsLib/installSoftware.py
+++ MolnsLib/installSoftware.py
@@ -86,7 +86,7 @@ class InstallSW:
             "cd /usr/local/StochKit && ./install.sh",
          
             #"wget https://github.com/StochSS/stochss/blob/master/ode-1.0.4.tgz?raw=true -q -O /tmp/ode.tgz",
-            "wget https://github.com/StochSS/StochKit_ode/archive/master.tar.gz?raw=true -q -O /tmp/ode.tgz"
+            "wget https://github.com/StochSS/StochKit_ode/archive/master.tar.gz?raw=true -q -O /tmp/ode.tgz",
             "cd /tmp && tar -xzf /tmp/ode.tgz",
             "sudo mv /tmp/StochKit_ode-master /usr/local/ode",
             "rm /tmp/ode.tgz",
